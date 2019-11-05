57da62edc8182702dcfc55494c8420f5785606d4
HactarCE <6060305+HactarCE@users.noreply.github.com>
2019-11-04 22:12:32 -0500

    Remove NdIndex stuff from crate::automaton

--- src/automaton/mod.rs
+++ src/automaton/mod.rs
@@ -2,13 +2,12 @@ mod algorithm;
 mod grid;
 
 use algorithm::Rule;
-use grid::{Cell, Grid};
+use grid::{Cell, Dimension, Grid};
 
-use ndarray::{Dimension, NdIndex};
 use std::marker::PhantomData;
 
-struct Automaton<C: Cell, D: Dimension, I: NdIndex<D>, G: Grid<C, D, I>, R: Rule<C, D>> {
-    phantom_data: PhantomData<(C, D, I)>,
+struct Automaton<C: Cell, D: Dimension, G: Grid<C, D>, R: Rule<C, D>> {
+    phantom: PhantomData<(C, D)>,
     grid: G,
     rule: R,
 }

469e9428cf1c2bc8d364ea3ea58c680c0290bfff
HactarCE <6060305+HactarCE@users.noreply.github.com>
2019-11-04 23:15:29 -0500

    Merge src/automaton/grid/mod.rs and src/automaton/grid/chunked_grid.rs

--- src/automaton/grid.rs
+++ src/automaton/grid.rs
@@ -1,7 +1,36 @@
 use ndarray::ArcArray;
+use std::clone::Clone;
+use std::cmp::Eq;
 use std::collections::HashMap;
+use std::default::Default;
+use std::hash::Hash;
 
-use super::{Cell, Dimension, Grid};
+/// A "trait alias" for ndarray::Dimension + std::cmp::Eq + std::hash::Hash so
+/// that it can be used in HashMaps.
+pub trait Dimension: ndarray::Dimension + Eq + Hash {}
+impl<T: ndarray::Dimension + Eq + Hash> Dimension for T {}
+
+/// A "trait alias" for a cell type that has a "default" value.
+pub trait Cell: Copy + Default + Eq {}
+impl<T: Copy + Default + Eq> Cell for T {}
+
+pub trait Grid<C: Cell, D: Dimension>: Clone {
+    /// Returns the number of dimensions in this grid.
+    fn ndim() -> usize {
+        D::NDIM.unwrap()
+    }
+
+    /// Returns the coordinates of the origin (0 on each axis).
+    fn origin() -> D {
+        D::zeros(Self::ndim())
+    }
+
+    /// Returns the cell at the given position.
+    fn get_cell(&self, index: D) -> C;
+
+    /// Sets the cell at the given position and returns the previous value.
+    fn set_cell(&mut self, index: D, cell_value: C) -> C;
+}
 
 #[derive(Clone)]
 struct ChunkedGrid<C: Cell, D: Dimension> {

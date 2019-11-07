db518ef5b5e752c9ce336a0c8cfc247a878d94c4
HactarCE <6060305+HactarCE@users.noreply.github.com>
2019-11-06 23:14:11 -0500

    Require Debug trait on Cell

--- src/automaton/space/mod.rs
+++ src/automaton/space/mod.rs
@@ -3,6 +3,7 @@
 
 use std::cmp::Eq;
 use std::default::Default;
+use std::fmt::Debug;
 use std::hash::Hash;
 
 mod grid;
@@ -20,5 +21,5 @@ impl<T: ndarray::Dimension + Eq + Hash> Dimension for T {}
 
 /// A "trait alias" for a cell type that has a "default" value and can be copied
 /// for free or near-free.
-pub trait Cell: Copy + Default + Eq {}
-impl<T: Copy + Default + Eq> Cell for T {}
+pub trait Cell: Debug + Copy + Default + Eq {}
+impl<T: Debug + Copy + Default + Eq> Cell for T {}

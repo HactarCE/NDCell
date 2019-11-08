2145d91df4fb91f498bf5efcb8453eb5974f9661
HactarCE <6060305+HactarCE@users.noreply.github.com>
2019-11-08 16:49:27 -0500

    Update Automaton struct and add a constructor

--- src/automaton/mod.rs
+++ src/automaton/mod.rs
@@ -14,6 +14,29 @@ use std::marker::PhantomData;
 /// A cellular automaton simulation.
 pub struct Automaton<T: CellType, C: Coords, A: Algorithm<T, C>> {
     phantom: PhantomData<(T, C)>,
-    grid: Grid<T, C>,
-    algorithm: A,
+    /// The algorithm to simulate.
+    pub algorithm: A,
+    /// The grid over which to simulate that algorithm.
+    pub grid: Grid<T, C>,
+}
+
+impl<T: CellType, C: Coords, A: Algorithm<T, C>> Automaton<T, C, A> {
+    /// Construct a new Automaton that simulates a given rule over a given grid.
+    pub fn new(algorithm: A, grid: Grid<T, C>) -> Self {
+        Self {
+            phantom: PhantomData,
+            algorithm,
+            grid,
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_cgol() {
+        // let sim = Automaton::new(algorithm::Totalistic());
+    }
 }

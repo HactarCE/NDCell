f8ac68a53bba0b27a606b308b0c753c356513c71
HactarCE <6060305+HactarCE@users.noreply.github.com>
2019-11-09 13:49:52 -0500

    impl fmt::Display for Chunk<bool, Coords2D>

--- src/automaton/space/chunk.rs
+++ src/automaton/space/chunk.rs
@@ -1,4 +1,6 @@
 use ndarray::ArcArray;
+use std::fmt;
+use std::fmt::Write;
 use std::ops::{Index, IndexMut};
 
 use super::*;
@@ -8,6 +10,20 @@ use super::*;
 pub struct Chunk<T: CellType, D: Dim> {
     /// The ndarray storing the cell contents of this chunk.
     pub array: ArcArray<T, D::NdarrayDim>,
+}
+
+impl fmt::Display for Chunk<bool, Coords2D> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        for ((_, x), &cell_value) in self.array.indexed_iter() {
+            f.write_char(if cell_value { '#' } else { '.' })?;
+            if x == Coords2D::CHUNK_SIZE - 1 {
+                f.write_char('\n')?;
+            } else {
+                f.write_char(' ')?;
+            }
+        }
+        Ok(())
+    }
 }
 
 impl<T: CellType, D: Dim> Chunk<T, D> {

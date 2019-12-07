a353e6d8bc7c51e75d472a0133680f7684c49e1a
HactarCE <6060305+HactarCE@users.noreply.github.com>
2019-12-07 02:51:32 -0500

    Document Zoom2D methods

--- src/ui/render/grid2d/zoom.rs
+++ src/ui/render/grid2d/zoom.rs
@@ -46,28 +46,58 @@ impl Zoom2D {
             Zoom2D::Close(n) => Zoom2D::Close(n / 2),
         }
     }
+
+    /// Returns the layer of the nodes that represent a single render cell.
+    ///
+    /// At zoom level 1:n (zoomed in), this is 1. At zoom level n:1 (zoomed
+    /// out), this is the log base 2 of n.
+    pub fn node_layer(self) -> usize {
+        match self {
+            Zoom2D::Close(_) => 0,
+            Zoom2D::Far(cell_pow) => cell_pow,
+        }
+    }
+
+    /// Returns the length of cells per screen pixel.
+    ///
+    /// At zoom level 1:n (zoomed in), this method returns n. At zoom level n:1
+    /// (zoomed out), this method returns 1/n.
     pub fn cells_per_pixel(self) -> f32 {
         match self {
             Zoom2D::Close(pixels) => 1.0 / pixels as f32,
             Zoom2D::Far(cell_pow) => 2.0f32.powf(cell_pow as f32),
         }
     }
+    /// Returns the length of cells per screen pixel.
+    ///
+    /// At zoom level 1:n (zoomed in), this method returns 1/n. At zoom level
+    /// n:1 (zoomed out), this method returns n.
     pub fn pixels_per_cell(self) -> f32 {
         match self {
             Zoom2D::Close(pixels) => pixels as f32,
             Zoom2D::Far(cell_pow) => 0.5f32.powf(cell_pow as f32),
         }
     }
+    /// Returns the length of the square of cells that is rendered as one unit.
+    ///
+    /// When zoomed in, each render cell is the same as a normal cell. When
+    /// zoomed out beyond 1:1, each render cell is a single pixel, which can
+    /// contain a large square of cells. For example, at zoom level 8:1, this
+    /// method returns `8`.
+    pub fn cells_per_render_cell(self) -> f32 {
+        match self {
+            Zoom2D::Close(_) => 1.0,
+            Zoom2D::Far(_) => self.cells_per_pixel(),
+        }
+    }
+    /// Returns the length of pixels that is rendered per cell, minimum one.
+    ///
+    /// This is the same as pixels_per_cell except that it is never lower than
+    /// 1.0.
     pub fn pixels_per_render_cell(self) -> usize {
         match self {
             Zoom2D::Close(pixels) => pixels,
             Zoom2D::Far(_) => 1,
-        }
-    }
-    pub fn node_layer(self) -> usize {
-        match self {
-            Zoom2D::Close(_) => 0,
-            Zoom2D::Far(cell_pow) => cell_pow,
         }
     }
 }

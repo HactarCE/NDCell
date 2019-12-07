77fcb5fc8f3df987f81d2bcfc7d31967dcd5cf2f
HactarCE <6060305+HactarCE@users.noreply.github.com>
2019-12-07 18:49:41 -0500

    Add many more methods to GridView

--- src/ui/render/mod.rs
+++ src/ui/render/mod.rs
@@ -2,6 +2,7 @@ mod grid2d;
 mod grid3d;
 
 use super::State;
+use crate::automaton::projection::*;
 pub use grid2d::*;
 pub use grid3d::*;
 
@@ -15,5 +16,68 @@ pub fn draw(state: &mut State, target: &mut glium::Frame) {
     match state.grid_view {
         GridView::Grid2D(ref mut render_state) => render_state.draw(target),
         GridView::Grid3D(ref mut render_state) => render_state.draw(target),
+    }
+}
+
+impl GridView {
+    pub fn get_sim_step_size(&self) -> usize {
+        match self {
+            GridView::Grid2D(render_state) => render_state.automaton.get_step_size(),
+            _ => unimplemented!(),
+        }
+    }
+    pub fn set_sim_step_size(&mut self, sim_step: usize) {
+        match self {
+            GridView::Grid2D(render_state) => render_state.automaton.set_step_size(sim_step),
+            _ => unimplemented!(),
+        }
+    }
+    pub fn step(&mut self) {
+        match self {
+            GridView::Grid2D(render_state) => render_state.automaton.step(),
+            _ => unimplemented!(),
+        }
+    }
+    pub fn step_single(&mut self) {
+        match self {
+            GridView::Grid2D(render_state) => render_state.automaton.step_single(),
+            _ => unimplemented!(),
+        }
+    }
+    pub fn has_undo(&mut self) -> bool {
+        match self {
+            GridView::Grid2D(render_state) => render_state.automaton.has_undo(),
+            _ => unimplemented!(),
+        }
+    }
+    pub fn has_redo(&mut self) -> bool {
+        match self {
+            GridView::Grid2D(render_state) => render_state.automaton.has_redo(),
+            _ => unimplemented!(),
+        }
+    }
+    pub fn undo(&mut self) -> bool {
+        match self {
+            GridView::Grid2D(render_state) => render_state.automaton.undo(),
+            _ => unimplemented!(),
+        }
+    }
+    pub fn redo(&mut self) -> bool {
+        match self {
+            GridView::Grid2D(render_state) => render_state.automaton.redo(),
+            _ => unimplemented!(),
+        }
+    }
+    pub fn reset(&mut self) -> bool {
+        match self {
+            GridView::Grid2D(render_state) => render_state.automaton.reset(),
+            _ => unimplemented!(),
+        }
+    }
+    pub fn push_to_history(&mut self) {
+        match self {
+            GridView::Grid2D(render_state) => render_state.automaton.push_to_undo_history(),
+            _ => unimplemented!(),
+        }
     }
 }

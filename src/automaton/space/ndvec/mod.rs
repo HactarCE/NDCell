fa121386bd3d9cdc369cf8e0873bd051ca758de6
HactarCE <6060305+HactarCE@users.noreply.github.com>
2020-01-16 22:50:49 -0500

    Remove old macros

--- src/automaton/space/ndvec/mod.rs
+++ src/automaton/space/ndvec/mod.rs
@@ -91,114 +91,11 @@ impl<D: DimFor<N>, N: NdVecNum> NdVec<D, N> {
     }
 }
 
-// make_ndvec_struct!(BigVec, BigInt, BigIntArray);
-// make_ndvec_struct!(FVec, R64, R64Array, true);
-// make_ndvec_struct!(IVec, isize, IsizeArray);
-// make_ndvec_struct!(UVec, usize, UsizeArray);
-// make_ndvec_struct!(ByteVec, u8, U8Array);
-
-// impl<D: Dim> Copy for FVec<D> {}
-// impl<D: Dim> Copy for IVec<D> {}
-// impl<D: Dim> Copy for UVec<D> {}
-// impl<D: Dim> Copy for ByteVec<D> {}
-
-// macro_rules! impl_ndvec_conversion {
-//     ($ndvec1:ident from $ndvec2:ident with $converter:expr) => {
-//         impl<D: Dim> From<$ndvec2<D>> for $ndvec1<D> {
-//             fn from(vec2: $ndvec2<D>) -> $ndvec1<D> {
-//                 let mut ret = $ndvec1::default();
-//                 for &ax in D::axes() {
-//                     ret[ax] = ($converter)(vec2[ax]);
-//                 }
-//                 ret
-//             }
-//         }
-//     };
-//     ($ndvec1:ident from $ndvec2:ident) => {
-//         impl_ndvec_conversion!($ndvec1 from $ndvec2 with std::convert::Into::into);
-//     };
-//     ($ndvec1:ident try_from $ndvec2:ident with $converter:expr) => {
-//         impl<D: Dim> TryFrom<&$ndvec2<D>> for $ndvec1<D> {
-//             type Error = ();
-//             fn try_from(vec2: &$ndvec2<D>) -> Result<$ndvec1<D>, ()> {
-//                 let mut ret = $ndvec1::default();
-//                 for &ax in D::axes() {
-//                     ret[ax] = ($converter)(&vec2[ax]).ok_or(())?
-//                 }
-//                 Ok(ret)
-//             }
-//         }
-//     };
-// }
-
-// // Convert to BigVec.
-// impl_ndvec_conversion!(BigVec try_from FVec with (|&x| FromPrimitive::from_f32(x)));
-// impl_ndvec_conversion!(BigVec from IVec);
-// impl_ndvec_conversion!(BigVec from ByteVec);
-// impl_ndvec_conversion!(BigVec from UVec);
-
-// // Convert to FVec.
-// impl_ndvec_conversion!(FVec try_from BigVec with ToPrimitive::to_f32);
-// impl_ndvec_conversion!(FVec from IVec with (|x| x as f32));
-// impl_ndvec_conversion!(FVec from UVec with (|x| x as f32));
-// impl_ndvec_conversion!(FVec from ByteVec);
-
-// // Convert to IVec.
-// impl_ndvec_conversion!(IVec try_from BigVec with ToPrimitive::to_isize);
-// impl_ndvec_conversion!(IVec try_from FVec with ToPrimitive::to_isize);
-// impl_ndvec_conversion!(IVec from ByteVec);
-// impl_ndvec_conversion!(IVec try_from UVec with ToPrimitive::to_isize);
-
-// // Convert to UVec.
-// impl_ndvec_conversion!(UVec try_from BigVec with ToPrimitive::to_usize);
-// impl_ndvec_conversion!(UVec try_from FVec with ToPrimitive::to_usize);
-// impl_ndvec_conversion!(UVec try_from IVec with ToPrimitive::to_usize);
-// impl_ndvec_conversion!(UVec from ByteVec);
-
-// /// BigVec constructor using array notation; e.g. bigvec![1, -2, 3] or
-// /// bigvec![-10; 2].
-// #[macro_export]
-// macro_rules! bigvec {
-//     ($($t:tt)*) => {
-//         BigVec([$($t)*])
-//     };
-// }
-// ///  IVec constructor using array notation; e.g. ivec![1, -2, 3] or ivec![-10;
-// /// 2].
-// #[macro_export]
-// macro_rules! ivec {
-//     ($($t:tt)*) => {
-//         IVec([$($t)*])
-//     };
-// }
-// ///  FVec constructor using array notation; e.g. fvec![1.1, -2.3, 3.0] or
-// /// fvec![-9.8; 2].
-// #[macro_export]
-// macro_rules! fvec {
-//     ($($t:tt)*) => {
-//         FVec([$($t)*])
-//     };
-// }
-// /// ByteVec constructor using array notation; e.g. bytevec![1, 2, 255] or
-// /// bytevec![30; 2].
-// #[macro_export]
-// macro_rules! bytevec {
-//     ($($t:tt)*) => {
-//         ByteVec([$($t)*])
-//     };
-// }
-// /// UVec constructor using array notation; e.g. uvec![1, 2, 1000] or uvec![30;
-// /// 2].
-// #[macro_export]
-// macro_rules! uvec {
-//     ($($t:tt)*) => {
-//         UVec([$($t)*])
-//     };
-// }
-
+/// NdVec constructor using array notation; e.g. ndvec![1, 2, 1000] or
+/// ndvec![30; 2].
 #[macro_export]
 macro_rules! ndvec {
-    ($($t:tt)*) => {NdVec([$($t)*])};
+    ($($t:tt)*) => { NdVec([$($t)*]) };
 }
 
 #[cfg(test)]

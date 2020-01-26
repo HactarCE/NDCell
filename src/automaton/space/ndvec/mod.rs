2fc1a9f47ae44d70abe625e919db8548a568a33b
HactarCE <6060305+HactarCE@users.noreply.github.com>
2020-01-25 20:23:03 -0500

    Add more NdVec conversion traits

--- src/automaton/space/ndvec/mod.rs
+++ src/automaton/space/ndvec/mod.rs
@@ -9,8 +9,8 @@
 //! have to deal with infinities and NaN, which really should NEVER show up in
 //! NdVecs.
 
-use noisy_float::prelude::R64;
-use num::{BigInt, Num, One, ToPrimitive, Zero};
+use noisy_float::prelude::{r64, R64};
+use num::{BigInt, FromPrimitive, Num, One, ToPrimitive, Zero};
 use std::cmp::Eq;
 use std::convert::TryInto;
 use std::fmt::Debug;
@@ -170,15 +170,25 @@ impl<D: DimFor<N>, N: NdVecNum> NdVec<D, N> {
     }
 }
 
-/// NdVecs that can be converted to UVecs.
+/// NdVecs that can be converted to UVecs but not using From/Into.
 pub trait AsUVec<D: Dim> {
     /// Converts the NdVec to a UVec, panicking if it does not fit.
     fn as_uvec(&self) -> UVec<D>;
 }
-/// NdVecs that can be converted to IVecs.
+/// NdVecs that can be converted to IVecs but not using From/Into.
 pub trait AsIVec<D: Dim> {
     /// Converts the NdVec to an IVec, panicking if it does not fit.
     fn as_ivec(&self) -> IVec<D>;
+}
+/// NdVecs that can be converted to FVecs but not using From/Into.
+pub trait AsFVec<D: Dim> {
+    /// Converts the NdVec to an FVec, panicking if it does not fit.
+    fn as_fvec(&self) -> FVec<D>;
+}
+/// NdVecs that can be converted to BigVecs but not using From/Into.
+pub trait AsBigVec<D: Dim> {
+    /// Converts the NdVec to an BigVec, panicking if it does not fit.
+    fn as_bigvec(&self) -> BigVec<D>;
 }
 
 impl<D: Dim> AsUVec<D> for IVec<D> {
@@ -207,6 +217,28 @@ impl<D: Dim> AsIVec<D> for BigVec<D> {
                 .to_isize()
                 .expect("Cannot convert such a large BigVec into an IVec")
         })
+    }
+}
+impl<D: Dim> AsIVec<D> for FVec<D> {
+    fn as_ivec(&self) -> IVec<D> {
+        IVec::from_fn(|ax| self[ax].raw() as isize)
+    }
+}
+
+impl<D: Dim> AsFVec<D> for BigVec<D> {
+    fn as_fvec(&self) -> FVec<D> {
+        FVec::from_fn(|ax| {
+            self[ax]
+                .to_f64()
+                .map(r64)
+                .expect("Cannot convert such a large BigVec into an FVec")
+        })
+    }
+}
+
+impl<D: Dim> AsBigVec<D> for FVec<D> {
+    fn as_bigvec(&self) -> BigVec<D> {
+        BigVec::from_fn(|ax| BigInt::from_f64(self[ax].raw()).unwrap())
     }
 }
 

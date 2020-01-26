625245cfb26e27f03646cb5b9275e50d0ca0e9a8
HactarCE <6060305+HactarCE@users.noreply.github.com>
2020-01-26 01:44:39 -0500

    Impl MulAssign and DivAssign for FVecs and FRects

--- src/automaton/space/ndrect/ops.rs
+++ src/automaton/space/ndrect/ops.rs
@@ -50,24 +50,34 @@ where
 
 // Integer multiplication is special, because bounds are inclusive.
 // Multiplication by a negative number panics.
-impl<D: DimFor<N>, N: NdVecNum, X: Copy> Mul<X> for NdRect<D, N>
+impl<D: DimFor<N>, N: NdVecNum, X> Mul<X> for NdRect<D, N>
 where
-    NdVec<D, N>: NdRectVec + Mul<X, Output = NdVec<D, N>>,
+    NdVec<D, N>: NdRectVec,
+    Self: MulAssign<X>,
 {
     type Output = Self;
     fn mul(self, operand: X) -> Self {
+        let mut ret = self;
+        ret *= operand;
+        ret
+    }
+}
+impl<D: DimFor<N>, N: NdVecNum, X: Copy> MulAssign<X> for NdRect<D, N>
+where
+    NdVec<D, N>: NdRectVec + MulAssign<X>,
+{
+    fn mul_assign(&mut self, operand: X) {
         // Call span() rather than constructing directly because if we multiply
         // by a negative number then the min and max might swap, so we need to
         // double-check the bounds.
-        let start = self.start * operand;
-        let size = self.size * operand;
+        self.start *= operand;
+        self.size *= operand;
         for &ax in D::Dim::axes() {
             assert!(
-                size[ax] > N::zero(),
+                self.size[ax] > N::zero(),
                 "Cannot multiply an NdRect by a negative value"
             );
         }
-        Self { start, size }
     }
 }
 
@@ -84,12 +94,24 @@ where
 }
 
 // Implement float division.
-impl<D: DimFor<N>, N: NdVecNum + Float, X: Copy> Div<X> for NdRect<D, N>
+impl<D: DimFor<N>, N: NdVecNum + Float, X> Div<X> for NdRect<D, N>
 where
-    NdVec<D, N>: NdRectVec + Div<X, Output = NdVec<D, N>>,
+    NdVec<D, N>: NdRectVec,
+    Self: DivAssign<X>,
 {
     type Output = Self;
     fn div(self, operand: X) -> Self {
-        Self::span(self.min() / operand, self.max() / operand)
+        let mut ret = self;
+        ret /= operand;
+        ret
+    }
+}
+impl<D: DimFor<N>, N: NdVecNum + Float, X: Copy> DivAssign<X> for NdRect<D, N>
+where
+    NdVec<D, N>: NdRectVec + DivAssign<X>,
+{
+    fn div_assign(&mut self, operand: X) {
+        self.start /= operand;
+        self.size /= operand;
     }
 }

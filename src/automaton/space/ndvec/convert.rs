3dbb755d8447ac895ea7a98ba59e6b51d529f21f
HactarCE <6060305+HactarCE@users.noreply.github.com>
2020-01-30 00:05:59 -0500

    Move NdVec conversion traits into their own file

---
+++ src/automaton/space/ndvec/convert.rs
@@ -0,0 +1,89 @@
+//! Several traits for converting between different types of NdVecs.
+
+use noisy_float::prelude::r64;
+use num::{FromPrimitive, ToPrimitive};
+use std::convert::TryInto;
+
+use super::*;
+
+/// NdVecs that can be converted to UVecs but not using From/Into.
+pub trait AsUVec<D: Dim> {
+    /// Converts the NdVec to a UVec, panicking if it does not fit.
+    fn as_uvec(&self) -> UVec<D>;
+}
+/// NdVecs that can be converted to IVecs but not using From/Into.
+pub trait AsIVec<D: Dim> {
+    /// Converts the NdVec to an IVec, panicking if it does not fit.
+    fn as_ivec(&self) -> IVec<D>;
+}
+/// NdVecs that can be converted to FVecs but not using From/Into.
+pub trait AsFVec<D: Dim> {
+    /// Converts the NdVec to an FVec, panicking if it does not fit.
+    fn as_fvec(&self) -> FVec<D>;
+}
+/// NdVecs that can be converted to BigVecs but not using From/Into.
+pub trait AsBigVec<D: Dim> {
+    /// Converts the NdVec to an BigVec, panicking if it does not fit.
+    fn as_bigvec(&self) -> BigVec<D>;
+}
+
+impl<D: Dim> AsUVec<D> for IVec<D> {
+    fn as_uvec(&self) -> UVec<D> {
+        UVec::from_fn(|ax| {
+            self[ax]
+                .try_into()
+                .expect("Cannot convert this IVec into a UVec")
+        })
+    }
+}
+impl<D: Dim> AsIVec<D> for UVec<D> {
+    fn as_ivec(&self) -> IVec<D> {
+        IVec::from_fn(|ax| {
+            self[ax]
+                .try_into()
+                .expect("Cannot convert this UVec into an IVec")
+        })
+    }
+}
+
+impl<D: Dim> AsIVec<D> for BigVec<D> {
+    fn as_ivec(&self) -> IVec<D> {
+        IVec::from_fn(|ax| {
+            self[ax]
+                .to_isize()
+                .expect("Cannot convert such a large BigVec into an IVec")
+        })
+    }
+}
+impl<D: Dim> AsIVec<D> for FVec<D> {
+    fn as_ivec(&self) -> IVec<D> {
+        IVec::from_fn(|ax| self[ax].raw() as isize)
+    }
+}
+
+impl<D: Dim> AsFVec<D> for BigVec<D> {
+    fn as_fvec(&self) -> FVec<D> {
+        FVec::from_fn(|ax| {
+            self[ax]
+                .to_f64()
+                .map(r64)
+                .expect("Cannot convert such a large BigVec into an FVec")
+        })
+    }
+}
+impl<D: Dim> AsFVec<D> for IVec<D> {
+    fn as_fvec(&self) -> FVec<D> {
+        FVec::from_fn(|ax| {
+            self[ax]
+                .to_f64()
+                .map(r64)
+                .expect("Cannot convert such a large BigVec into an FVec")
+        })
+    }
+}
+
+impl<D: Dim> AsBigVec<D> for FVec<D> {
+    fn as_bigvec(&self) -> BigVec<D> {
+        BigVec::from_fn(|ax| BigInt::from_f64(self[ax].raw()).unwrap())
+    }
+}
--- src/automaton/space/ndvec/mod.rs
+++ src/automaton/space/ndvec/mod.rs
@@ -9,22 +9,23 @@
 //! have to deal with infinities and NaN, which really should NEVER show up in
 //! NdVecs.
 
-use noisy_float::prelude::{r64, R64};
-use num::{BigInt, FromPrimitive, Num, One, ToPrimitive, Zero};
+use noisy_float::prelude::R64;
+use num::{BigInt, Num, One, Zero};
 use std::cmp::Eq;
-use std::convert::TryInto;
 use std::fmt::Debug;
 use std::hash::Hash;
 use std::ops::*;
 
 mod aliases;
 mod axis;
+mod convert;
 mod dim;
 mod ops;
 
 pub use aliases::*;
 pub use axis::Axis::{U, V, W, X, Y, Z};
 pub use axis::*;
+pub use convert::*;
 pub use dim::*;
 
 /// A "trait alias" for types that can be used as coordinates in an NdVec.
@@ -167,88 +168,6 @@ impl<D: DimFor<N>, N: NdVecNum> NdVec<D, N> {
             ret *= self[ax].clone();
         }
         ret
-    }
-}
-
-/// NdVecs that can be converted to UVecs but not using From/Into.
-pub trait AsUVec<D: Dim> {
-    /// Converts the NdVec to a UVec, panicking if it does not fit.
-    fn as_uvec(&self) -> UVec<D>;
-}
-/// NdVecs that can be converted to IVecs but not using From/Into.
-pub trait AsIVec<D: Dim> {
-    /// Converts the NdVec to an IVec, panicking if it does not fit.
-    fn as_ivec(&self) -> IVec<D>;
-}
-/// NdVecs that can be converted to FVecs but not using From/Into.
-pub trait AsFVec<D: Dim> {
-    /// Converts the NdVec to an FVec, panicking if it does not fit.
-    fn as_fvec(&self) -> FVec<D>;
-}
-/// NdVecs that can be converted to BigVecs but not using From/Into.
-pub trait AsBigVec<D: Dim> {
-    /// Converts the NdVec to an BigVec, panicking if it does not fit.
-    fn as_bigvec(&self) -> BigVec<D>;
-}
-
-impl<D: Dim> AsUVec<D> for IVec<D> {
-    fn as_uvec(&self) -> UVec<D> {
-        UVec::from_fn(|ax| {
-            self[ax]
-                .try_into()
-                .expect("Cannot convert this IVec into a UVec")
-        })
-    }
-}
-impl<D: Dim> AsIVec<D> for UVec<D> {
-    fn as_ivec(&self) -> IVec<D> {
-        IVec::from_fn(|ax| {
-            self[ax]
-                .try_into()
-                .expect("Cannot convert this UVec into an IVec")
-        })
-    }
-}
-
-impl<D: Dim> AsIVec<D> for BigVec<D> {
-    fn as_ivec(&self) -> IVec<D> {
-        IVec::from_fn(|ax| {
-            self[ax]
-                .to_isize()
-                .expect("Cannot convert such a large BigVec into an IVec")
-        })
-    }
-}
-impl<D: Dim> AsIVec<D> for FVec<D> {
-    fn as_ivec(&self) -> IVec<D> {
-        IVec::from_fn(|ax| self[ax].raw() as isize)
-    }
-}
-
-impl<D: Dim> AsFVec<D> for BigVec<D> {
-    fn as_fvec(&self) -> FVec<D> {
-        FVec::from_fn(|ax| {
-            self[ax]
-                .to_f64()
-                .map(r64)
-                .expect("Cannot convert such a large BigVec into an FVec")
-        })
-    }
-}
-impl<D: Dim> AsFVec<D> for IVec<D> {
-    fn as_fvec(&self) -> FVec<D> {
-        FVec::from_fn(|ax| {
-            self[ax]
-                .to_f64()
-                .map(r64)
-                .expect("Cannot convert such a large BigVec into an FVec")
-        })
-    }
-}
-
-impl<D: Dim> AsBigVec<D> for FVec<D> {
-    fn as_bigvec(&self) -> BigVec<D> {
-        BigVec::from_fn(|ax| BigInt::from_f64(self[ax].raw()).unwrap())
     }
 }
 

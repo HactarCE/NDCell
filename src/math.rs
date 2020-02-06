86acb2aa4d421c32f71ee4d2915d5bcd6a3b6992
HactarCE <6060305+HactarCE@users.noreply.github.com>
2020-02-06 18:24:24 -0500

    Clean up and fix line-drawing routine

--- src/math.rs
+++ src/math.rs
@@ -27,59 +27,79 @@ pub fn round_to(n: f32, m: f32) -> f32 {
 /// Returns an iterator that uses a generalized version of Bresenham's
 /// line-drawing algorithm to return the integer points on a line between the
 /// given points.
-pub fn bresenham<D: Dim>(start: BigVec<D>, end: BigVec<D>) -> impl Iterator<Item = BigVec<D>> {
-    let delta = &end - &start;
+///
+/// TODO: comment this method!
+pub fn bresenham<D: Dim>(
+    mut start: BigVec<D>,
+    mut end: BigVec<D>,
+) -> impl Iterator<Item = BigVec<D>> {
+    let mut delta = &end - &start;
     let longest_axis = delta.max_axis(|_, val| val.abs());
-    let longest_axis_range =
-        num::range_inclusive(start[longest_axis].clone(), end[longest_axis].clone());
+    // Ensure that delta[longest_axis] is positive.
+    let negate = delta[longest_axis].is_negative();
+    if negate {
+        start = -start;
+        end = -end;
+        delta = -delta;
+    }
+    let longest_axis_range = num::range_inclusive(BigInt::from(0), delta[longest_axis].clone());
     let double_delta = &delta * &BigInt::from(2);
     let double_longest_axis_len = double_delta[longest_axis].clone();
     let mut current = start;
-    let mut error = BigVec::repeat(delta[longest_axis].clone()) * delta.signum();
-    // error[longest_axis] = BigInt::from(0); // TODO: explain why this is necessary
+    let mut error = delta.signum() * BigVec::repeat(delta[longest_axis].clone());
     longest_axis_range.map(move |_| {
         let ret = current.clone();
-        println!("error was {}", error);
-        error += &double_delta;
-        println!(
-            "next one will gain {}",
-            error.div_to_zero(&double_longest_axis_len)
-        );
-        current += error.div_to_zero(&double_longest_axis_len);
-        error = error.rem_to_zero(&double_longest_axis_len);
-        ret
+        if ret != end {
+            error += &double_delta;
+            current += error.div_to_zero(&double_longest_axis_len);
+            error = error.rem_to_zero(&double_longest_axis_len);
+        }
+        if negate {
+            -ret
+        } else {
+            ret
+        }
     })
 }
 
 #[cfg(test)]
 mod tests {
+    use noisy_float::prelude::r64;
     use proptest::prelude::*;
 
     use super::*;
     use crate::automaton::space::*;
 
     fn test_bresenham_single_line<D: Dim>(start: IVec<D>, end: IVec<D>) {
-        let big_start: BigVec<_> = start.convert();
-        let big_end: BigVec<_> = end.convert();
+        let big_start: BigVec<D> = start.convert();
+        let big_end: BigVec<D> = end.convert();
+
+        if big_start == big_end {
+            let points: Vec<BigVec<D>> = bresenham(big_start.clone(), big_end).collect();
+            assert_eq!(vec![big_start], points);
+            return;
+        }
+
         let rect = NdRect::span(big_start.clone(), big_end.clone());
 
-        let f_start: FVec<_> = start.as_fvec();
-        let f_end: FVec<_> = end.as_fvec();
+        let f_start: FVec<D> = start.as_fvec();
+        let f_end: FVec<D> = end.as_fvec();
 
         let mut delta = f_end - f_start.clone();
         let longest_axis = delta.max_axis(|_, val| val.abs());
-        delta /= delta[longest_axis];
+        delta /= delta[longest_axis].abs();
         let mut f_current = f_start.clone();
         let mut max_positive_error = r64(0.0);
         let mut max_negative_error = r64(0.0);
-        for p in bresenham(big_start, big_end) {
+        let mut last_point = None;
+        for p in bresenham(big_start, big_end.clone()) {
             println!("integer {}", p);
             println!("float   {}", &f_current);
             let diff = p.as_fvec() - f_current.clone();
             println!("diff    {}", diff);
             println!();
 
-            // assert!(rect.contains(&p));
+            assert!(rect.contains(&p));
 
             max_positive_error =
                 std::cmp::max(max_positive_error, diff[diff.max_axis(|_, val| *val)]);
@@ -87,11 +107,15 @@ mod tests {
                 std::cmp::min(max_negative_error, diff[diff.min_axis(|_, val| *val)]);
 
             f_current += delta.clone();
+
+            last_point = Some(p);
         }
         println!("max positive error = {}", max_positive_error);
         println!("max negative error = {}", max_negative_error);
         assert!(max_positive_error < 0.51);
         assert!(max_negative_error > -0.51);
+
+        assert_eq!(Some(big_end), last_point, "Line did not complete");
     }
 
     proptest! {

61d9218b605a6bac4e5f4c23cb85b0f696899440
HactarCE <6060305+HactarCE@users.noreply.github.com>
2020-05-25 17:02:07 -0400

    Add test for function call errors

--- lang/src/tests/funcs.rs
+++ lang/src/tests/funcs.rs
@@ -1,4 +1,35 @@
-use super::assert_fn_compile_error;
+use super::{assert_compile_error, assert_fn_compile_error};
+
+// Test errors related to calling functions.
+#[test]
+fn test_function_errors() {
+    // Function lookup error
+    let source_code = "
+        @function int test(int y) {
+            return func_doesnt_exist(3, y)
+        }
+        @function int other_func(int x, int y) {
+            return x + y
+        }
+    ";
+    let expected = ("func_doesnt_exist", "There is no function with this name");
+    assert_compile_error(source_code, expected);
+
+    // Bad arguments
+    let source_code = "
+        @function int test(int y) {
+            return other_func([1, 2], y)
+        }
+        @function int other_func(int x, int y) {
+            return x + y
+        }
+    ";
+    let expected = (
+        "other_func([1, 2], y)",
+        r#"Invalid arguments [vec2, int] for helper function "other_func"; expected [int, int]"#,
+    );
+    assert_compile_error(source_code, expected);
+}
 
 // Calling user functions is not supported (yet), but make sure that it doesn't
 // panic or anything.

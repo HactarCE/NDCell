6003260cbc44b3fbb3ed416b2fc3e9279d2998da
HactarCE <6060305+HactarCE@users.noreply.github.com>
2020-05-31 22:53:47 -0400

    Add tests for bitwise ops and bitshifts

--- lang/src/tests/math.rs
+++ lang/src/tests/math.rs
@@ -1,4 +1,5 @@
 use proptest::prelude::*;
+use std::convert::TryInto;
 
 use super::{
     assert_fn_result, assert_threadlocal_fn_result, compile_test_fn, test_values, CompiledFunction,
@@ -18,6 +19,20 @@ thread_local! {
         compile_test_fn("@function int test(int x, int y) { return x % y }");
     static NEG_FN: CompiledFunction =
         compile_test_fn("@function int test(int x, int y) { return -x }");
+
+    static BIT_OR_FN: CompiledFunction =
+        compile_test_fn("@function int test(int x, int y) { return x | y }");
+    static BIT_XOR_FN: CompiledFunction =
+        compile_test_fn("@function int test(int x, int y) { return x ^ y }");
+    static BIT_AND_FN: CompiledFunction =
+        compile_test_fn("@function int test(int x, int y) { return x & y }");
+
+    static LSH_FN: CompiledFunction =
+        compile_test_fn("@function int test(int x, int y) { return x << y }");
+    static RSH_ARITH_FN: CompiledFunction =
+        compile_test_fn("@function int test(int x, int y) { return x >> y }");
+    static RSH_LOGIC_FN: CompiledFunction =
+        compile_test_fn("@function int test(int x, int y) { return x >>> y }");
 }
 
 // Test with random inputs.
@@ -55,41 +70,72 @@ fn test_arithmetic(x: LangInt, y: LangInt) {
         .map(ConstValue::Int)
         .ok_or(("x + y", overflow_err_msg));
     assert_threadlocal_fn_result(&ADD_FN, &args, expected);
-
     // Subtraction
     let expected = x
         .checked_sub(y)
         .map(ConstValue::Int)
         .ok_or(("x - y", overflow_err_msg));
     assert_threadlocal_fn_result(&SUB_FN, &args, expected);
-
     // Multiplication
     let expected = x
         .checked_mul(y)
         .map(ConstValue::Int)
         .ok_or(("x * y", overflow_err_msg));
     assert_threadlocal_fn_result(&MUL_FN, &args, expected);
-
     // Division
     let expected = x
         .checked_div(y)
         .map(ConstValue::Int)
         .ok_or(("x / y", div_err_msg));
     assert_threadlocal_fn_result(&DIV_FN, &args, expected);
-
     // Remainder
     let expected = x
         .checked_rem(y)
         .map(ConstValue::Int)
         .ok_or(("x % y", div_err_msg));
     assert_threadlocal_fn_result(&MOD_FN, &args, expected);
-
     // Negation
     let expected = x
         .checked_neg()
         .map(ConstValue::Int)
         .ok_or(("-x", overflow_err_msg));
     assert_threadlocal_fn_result(&NEG_FN, &args, expected);
+
+    // Bitwise OR
+    let expected = Ok(ConstValue::Int(x | y));
+    assert_threadlocal_fn_result(&BIT_OR_FN, &args, expected);
+    // Bitwise XOR
+    let expected = Ok(ConstValue::Int(x ^ y));
+    assert_threadlocal_fn_result(&BIT_XOR_FN, &args, expected);
+    // Bitwise AND
+    let expected = Ok(ConstValue::Int(x & y));
+    assert_threadlocal_fn_result(&BIT_AND_FN, &args, expected);
+
+    // Shift left
+    let expected = y
+        .try_into()
+        .ok()
+        .and_then(|y| x.checked_shl(y))
+        .map(ConstValue::Int)
+        .ok_or(("x << y", div_err_msg));
+    assert_threadlocal_fn_result(&LSH_FN, &args, expected);
+    // Shift right arithmetic
+    let expected = y
+        .try_into()
+        .ok()
+        .and_then(|y| x.checked_shr(y))
+        .map(ConstValue::Int)
+        .ok_or(("x >> y", div_err_msg));
+    assert_threadlocal_fn_result(&RSH_ARITH_FN, &args, expected);
+    // Shift right logical
+    let expected = y
+        .try_into()
+        .ok()
+        .and_then(|y| (x as u64).checked_shr(y))
+        .map(|i| i as LangInt)
+        .map(ConstValue::Int)
+        .ok_or(("x >>> y", div_err_msg));
+    assert_threadlocal_fn_result(&RSH_LOGIC_FN, &args, expected);
 }
 
 #[test]

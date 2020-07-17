fda5803beeaa9adfaa87de34c4ec0856c5b8d1b4
HactarCE <6060305+HactarCE@users.noreply.github.com>
2020-07-17 01:43:36 -0400

    Impl Rule for CompiledFunction

--- lang/src/lib.rs
+++ lang/src/lib.rs
@@ -5,9 +5,7 @@
 #[macro_use]
 extern crate lazy_static;
 
-// use std::fs::File;
-// use std::io::Read;
-use std::rc::Rc;
+use std::sync::{mpsc, Arc};
 
 #[macro_use]
 mod utils;
@@ -42,47 +40,73 @@ pub const MAX_STATE_COUNT: usize = 256;
 pub const MAX_PATTERN_SIZE: usize = 4096;
 
 /// Number of dimensions to use when the user doesn't specify.
-const DEFAULT_NDIM: usize = 2;
+pub const DEFAULT_NDIM: usize = 2;
 /// Neighborhood radius to use when the user doesn't specify.
-const DEFAULT_NBHD_RADIUS: usize = 1;
+pub const DEFAULT_NBHD_RADIUS: usize = 1;
 /// Number of states to use when the user doesn't specify.
-const DEFAULT_STATE_COUNT: usize = 2;
+pub const DEFAULT_STATE_COUNT: usize = 2;
 
 /// Reserved throwaway variable name.
 const THROWAWAY_VARIABLE: &'static str = "_";
 
-// fn main() -> Result<(), ()> {
-//     let args: Vec<String> = std::env::args().collect();
-//     if args.len() != 2 {
-//         eprintln!(
-//             "\
-// Please specify a file to run. E.g.
+fn compile_rule(
+    mpsc_sender: &mpsc::Sender<LangResult<compiler::CompiledFunction>>,
+    source_code: Arc<String>,
+    fn_name: Option<String>,
+) -> LangResult<()> {
+    let rule = ast::make_rule(source_code.clone())?;
+    let compiler = compiler::Compiler::new()?;
+    match fn_name {
+        Some(s) => &rule.helper_functions()[&s],
+        None => rule.transition_function(),
+    }
+    .compile(mpsc_sender, compiler)?;
+    Ok(())
+}
 
-// {} examples{}life.ndca",
-//             args[0],
-//             std::path::MAIN_SEPARATOR,
-//         );
-//         Err(())?;
-//     }
+/// Compiles the specified function of the given source code. If the function
+/// name is None, then the transition function is returned.
+pub fn compile_blocking(
+    source_code: Arc<String>,
+    fn_name: Option<String>,
+) -> LangResult<compiler::CompiledFunction> {
+    let (tx, rx) = mpsc::channel();
+    std::thread::spawn(move || {
+        if let Err(e) = compile_rule(&tx, source_code, fn_name) {
+            tx.send(Err(e)).expect("MPSC channel error");
+        }
+    });
+    rx.recv()
+        .unwrap_or_else(|e| internal_error!("MPSC channel error: {:?}", e))
+}
 
-//     let filename = &args[1];
-//     let mut file = File::open(filename).expect("Error opening file");
-//     let mut source_code = String::new();
-//     file.read_to_string(&mut source_code)
-//         .expect("Error reading file");
-//     let source_code = Rc::new(source_code);
-
-//     let result = compile_and_run(source_code.clone());
-//     match result {
-//         Ok(ret) => println!("JIT-compiled transition function output: {:?}", ret),
-//         Err(err) => {
-//             eprintln!("{}", err.with_source(&source_code));
-//             Err(())?
-//         }
-//     }
-
-//     Ok(())
-// }
+impl ndcell_core::Rule<u8, ndcell_core::Dim2D> for compiler::CompiledFunction {
+    fn radius(&self) -> usize {
+        let (min, max) = self.rule_meta().nbhd_shape.bounds().min_and_max();
+        min.into_iter()
+            .zip(max)
+            .map(|(lo, hi)| std::cmp::max(lo.abs(), hi.abs()) as usize)
+            .max()
+            .unwrap()
+    }
+    fn get_transition_function(&self) -> ndcell_core::TransitionFunction<u8, ndcell_core::Dim2D> {
+        let mut compiled_func = self.clone();
+        Box::new(move |neighborhood| {
+            let nbhd_pattern = types::Pattern {
+                cells: compiled_func
+                    .rule_meta()
+                    .nbhd_shape
+                    .bounds()
+                    .iter()
+                    .map(|pos| neighborhood[&ndcell_core::Vec2D::from_fn(|ax| pos[ax as usize])])
+                    .collect(),
+                shape: compiled_func.rule_meta().nbhd_shape.clone(),
+                lut: None,
+            };
+            compiled_func.call(&mut [ConstValue::Pattern(nbhd_pattern)]).expect("Runtime error in transition function! And I haven't implemented proper error handling yet! AAAHHH").as_cell_state().unwrap()
+        })
+    }
+}
 
 #[cfg(test)]
 #[macro_use]

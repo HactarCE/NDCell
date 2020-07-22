6194dec8066a6564193914b03cb9be1a8f25f848
HactarCE <6060305+HactarCE@users.noreply.github.com>
2020-07-22 00:07:06 -0400

    Customize Sphinx docs, use RTD theme, and add icon

--- docs/conf.py
+++ docs/conf.py
@@ -14,6 +14,8 @@
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
+import sphinx_rtd_theme
+
 
 # -- Project information -----------------------------------------------------
 
@@ -28,6 +30,7 @@ author = 'HactarCE'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    'sphinx_rtd_theme',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -44,9 +47,16 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+html_theme_options = {
+    'sticky_navigation': False,
+    'html_logo': 'img/ndcell_icon.svg',
+}
+
+master_doc = 'index'

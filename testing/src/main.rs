f39dcd0b7f5dbe5842514159d28b505d23fb70fc
HactarCE <6060305+HactarCE@users.noreply.github.com>
2020-09-02 10:44:49 -0400

    Add informal benchmarking to testing/src/main.rs

--- testing/src/main.rs
+++ testing/src/main.rs
@@ -1,14 +1,17 @@
 use ndcell_core::prelude::*;
+use std::time::Instant;
 
 fn main() {
-    println!("heya");
-    // let gotts_dots = Automaton2D::from_rle(
-    //     "x = 187, y = 39, rule = B3/S23
-    //     o$o$o9$4bo3bo$5bobo$6bo2bo$9bo$9bo9$185bo$186bo$182bo3bo$183b4o$179bo$
-    //     180b2o$179bo$183b4o$182bo3bo$186bo$185bo$175bo$176bo$170bo5bo$171b6o!",
-    // )
-    // .unwrap();
-    // run_simulation(&gotts_dots, 64, 4);
+    let gotts_dots = Automaton2D::from_rle(
+        "x = 187, y = 39, rule = B3/S23
+        o$o$o9$4bo3bo$5bobo$6bo2bo$9bo$9bo9$185bo$186bo$182bo3bo$183b4o$179bo$
+        180b2o$179bo$183b4o$182bo3bo$186bo$185bo$175bo$176bo$170bo5bo$171b6o!",
+    )
+    .unwrap();
+    let start = Instant::now();
+    run_simulation(&gotts_dots, 64, 4);
+    let end = Instant::now();
+    println!("{:?}", end - start);
 }
 
 fn run_simulation(
